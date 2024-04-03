1c1d1e6748d57cc05ad67607329f4b78acaccb95
rnjsrkdud0524 <141720031+rnjsrkdud0524@users.noreply.github.com>
2024-04-03 17:35:31 +0900

    Add files via upload

--- Phone_Prj/Phone_Prj/urls.py
+++ Phone_Prj/Phone_Prj/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from contacts import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.list, name='list'),
+    path('result/', views.result, name='result'),
 ]
